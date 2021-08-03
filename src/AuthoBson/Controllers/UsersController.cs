29b2174ef3a3e4a811cd2be836c613f25959bb53
Pomidorka1234 <Jansafronov@gmail.com>
2021-08-03 18:50:59 +0300

    Commit

---
+++ src/AuthoBson/Controllers/UsersController.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Text;
+using System.Transactions;
+using System.Web;
+using System.Security;
+using System.Security.Authentication;
+using System.Security.Authentication.ExtendedProtection;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.AspNetCore.Session;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using AuthoBson.Models;
+using AuthoBson.Services;
+using AuthoBson.Services.Security;
+
+
+namespace AuthoBson.Controllers {
+
+    [ApiController]
+    [Route("api/[controller]", Name = "User")]
+    public class UserController : ControllerBase {
+
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<BsonUser>> Get() =>
+            _userService.GetAll().ToList();
+
+        [HttpGet("{id:length(24)}", Name = "GetUser")]
+        public ActionResult<BsonUser> Get(string id) {
+            BsonUser user = _userService.GetUser(id);
+
+            if (user == null) {
+                return NotFound(user);
+            }
+            
+            return user;
+        }
+
+        [HttpPost]
+        public ActionResult<BsonUser> Create(BsonUser user)
+        {
+            GenericHash hash = GenericHash.Encode<SHA256>(user.password.AsString, 8);
+
+            user.password = Convert.ToBase64String(hash.Salt) + Convert.ToBase64String(hash.Passhash);
+
+            return CreatedAtRoute("GetUser", new { id = user.Id.ToString() }, user);
+        }
+
+        [Authorize(Policy = "moderate")]
+        [HttpPost("{id:length(24)}")]
+        public IActionResult Suspend(BsonUser initiator, string id, string reason, DateTime duration) {
+            if (!_userService.Moderate(initiator))
+                return new UnauthorizedObjectResult(initiator.username + " is not authorized to do this action");
+
+            if (_userService.SuspendUser(id, reason, duration) == null)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, BsonUser userIn)
+        {
+            var user = _userService.GetUser(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _userService.ReplaceUser(id, userIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var user = _userService.GetUser(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _userService.RemoveUser(user.Id.AsString);
+
+            return NoContent();
+        }
+
+        
+    }
+}
---
+++ src/AuthoBson/Services/UserService.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using MongoDB;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+using MongoDB.Driver.Core.Clusters;
+using MongoDB.Driver.Core.Connections;
+using MongoDB.Driver.Core.Events;
+using MongoDB.Driver.Core.Events.Diagnostics;
+using MongoDB.Driver.Core.Operations;
+using MongoDB.Driver.Encryption;
+using MongoDB.Driver.GeoJsonObjectModel;
+using MongoDB.Driver.Linq;
+using AuthoBson.Models;
+
+namespace AuthoBson.Services {
+    public class UserService {
+
+        private IMongoCollection<BsonUser> _users { get; }
+
+        public UserService (IUserstoreDatabaseSettings settings) {
+            MongoClient client = new MongoClient(settings.ConnectionString);
+            IMongoDatabase database = client.GetDatabase(settings.DatabaseName);
+
+            _users = database.GetCollection<BsonUser>(settings.UsersCollectionName);
+        }
+
+        /// <summary>
+        /// Returns the Users enumerable collection
+        /// </summary>
+        /// <returns>Users enumerable collection</returns>
+        public IEnumerable<BsonUser> GetAll () => _users.Find(user => true).ToEnumerable();
+
+        /// <summary>
+        /// Finds a User from an enumerable collection by username
+        /// </summary>
+        /// <param name="id">Id of the user to find</param>
+        /// <returns>User object or null</returns>
+        public BsonUser GetUser (string id) => _users.Find<BsonUser>(user => user.Id == id).FirstOrDefault();
+
+        public BsonUser CreateUser (BsonUser user) {
+            _users.InsertOne(user);
+            return user;
+        }
+
+        public BsonUser ReplaceUser (string id, BsonUser newuser) {
+            _users.ReplaceOne(user => user.Id == id, newuser);
+            return newuser;
+        }
+
+        public bool Moderate (BsonUser user) {
+            bool proof = user.role >= Role.Moderator;
+            proof = proof && user.suspended.duration == null;
+            
+            return proof;
+        }
+
+        public BsonUser SuspendUser (string id, string reason, DateTime duration) {
+            UpdateDefinitionBuilder<BsonUser> bupdate = new UpdateDefinitionBuilder<BsonUser>();
+            UpdateDefinition<BsonUser> update = bupdate.AddToSet("reason", reason).AddToSet("duration", duration);
+
+            return _users.FindOneAndUpdate<BsonUser>(user => user.Id == id, update);
+        }
+
+        public BsonUser RemoveUser (string id) => _users.FindOneAndDelete(user => user.Id == id);
+
+        /// <summary>
+        /// Morph a bson field by bsontype and by value as an argument of a function
+        /// </summary>
+        /// <param name="id">Id of the user to find</param>
+        /// <param name="key">Key of the field to change</param>
+        /// <param name="functor">Endomorphic mapping between the type of the field</param>
+        /// <typeparam name="B">BsonValue</typeparam>
+        /// <returns></returns>
+        public BsonUser ChangeField<B> (string id, string key, Func<BsonValue, BsonValue> functor) {
+            BsonUserDocument doc = new BsonUserDocument(this.GetUser(id));
+            doc = doc.functor<B>(key, functor);
+            return BsonSerializer.Deserialize<BsonUser>(doc.user);
+        }
+    }
+}
