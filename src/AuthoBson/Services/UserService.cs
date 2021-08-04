76337418b4f32fe0af6719d64ef200d38b66d910
Pomidorka1234 <Jansafronov@gmail.com>
2021-08-05 02:19:50 +0300

    Commit to Unit Tests

--- src/AuthoBson/Services/UserService.cs
+++ src/AuthoBson/Services/UserService.cs
@@ -25,53 +25,46 @@ using AuthoBson.Models;
 namespace AuthoBson.Services {
     public class UserService {
 
-        private IMongoCollection<BsonUser> _users { get; }
+        private IMongoCollection<User> _users { get; }
 
         public UserService (IUserstoreDatabaseSettings settings) {
             MongoClient client = new MongoClient(settings.ConnectionString);
             IMongoDatabase database = client.GetDatabase(settings.DatabaseName);
 
-            _users = database.GetCollection<BsonUser>(settings.UsersCollectionName);
+            _users = database.GetCollection<User>(settings.UsersCollectionName);
         }
 
         /// <summary>
         /// Returns the Users enumerable collection
         /// </summary>
         /// <returns>Users enumerable collection</returns>
-        public IEnumerable<BsonUser> GetAll () => _users.Find(user => true).ToEnumerable();
+        public IEnumerable<User> GetAll () => _users.Find(user => true).ToEnumerable();
 
         /// <summary>
         /// Finds a User from an enumerable collection by username
         /// </summary>
         /// <param name="id">Id of the user to find</param>
         /// <returns>User object or null</returns>
-        public BsonUser GetUser (string id) => _users.Find<BsonUser>(user => user.Id == id).FirstOrDefault();
+        public User GetUser (string id) => _users.Find<User>(user => user.Id == id).FirstOrDefault();
 
-        public BsonUser CreateUser (BsonUser user) {
+        public User CreateUser (User user) {
             _users.InsertOne(user);
             return user;
         }
 
-        public BsonUser ReplaceUser (string id, BsonUser newuser) {
+        public User ReplaceUser (string id, User newuser) {
             _users.ReplaceOne(user => user.Id == id, newuser);
             return newuser;
         }
 
-        public bool Moderate (BsonUser user) {
-            bool proof = user.role >= Role.Moderator;
-            proof = proof && user.suspended.duration == null;
-            
-            return proof;
+        public User SuspendUser (string id, string reason, DateTime duration) {
+            UpdateDefinitionBuilder<User> bupdate = new UpdateDefinitionBuilder<User>();
+            UpdateDefinition<User> update = bupdate.AddToSet("reason", reason).AddToSet("duration", duration);
+
+            return _users.FindOneAndUpdate<User>(user => user.Id == id, update);
         }
 
-        public BsonUser SuspendUser (string id, string reason, DateTime duration) {
-            UpdateDefinitionBuilder<BsonUser> bupdate = new UpdateDefinitionBuilder<BsonUser>();
-            UpdateDefinition<BsonUser> update = bupdate.AddToSet("reason", reason).AddToSet("duration", duration);
-
-            return _users.FindOneAndUpdate<BsonUser>(user => user.Id == id, update);
-        }
-
-        public BsonUser RemoveUser (string id) => _users.FindOneAndDelete(user => user.Id == id);
+        public User RemoveUser (string id) => _users.FindOneAndDelete(user => user.Id == id);
 
         /// <summary>
         /// Morph a bson field by bsontype and by value as an argument of a function
@@ -81,10 +74,10 @@ namespace AuthoBson.Services {
         /// <param name="functor">Endomorphic mapping between the type of the field</param>
         /// <typeparam name="B">BsonValue</typeparam>
         /// <returns></returns>
-        public BsonUser ChangeField<B> (string id, string key, Func<BsonValue, BsonValue> functor) {
-            BsonUserDocument doc = new BsonUserDocument(this.GetUser(id));
+        public User ChangeField<B> (string id, string key, Func<BsonValue, BsonValue> functor) {
+            UserDocument doc = new UserDocument(this.GetUser(id));
             doc = doc.functor<B>(key, functor);
-            return BsonSerializer.Deserialize<BsonUser>(doc.user);
+            return BsonSerializer.Deserialize<User>(doc.user);
         }
     }
 }
