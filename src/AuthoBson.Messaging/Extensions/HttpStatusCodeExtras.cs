13905f8fb4526cebe40ef8ccb8b7d8a91f656849
Jan <72037282+Pomid0rchik@users.noreply.github.com>
2021-10-07 02:46:31 +0300

    Update HttpStatusCodeExtras.cs

--- src/AuthoBson.Messaging/Extensions/HttpStatusCodeExtras.cs
+++ src/AuthoBson.Messaging/Extensions/HttpStatusCodeExtras.cs
@@ -2,7 +2,6 @@ namespace AuthoBson.Messaging.Extensions
 {
     public enum HttpStatusCodeExtras
     {
-        // Add your custom status codes here, e.g.:
-        // InvalidPassword = 470
+        
     }
 }
