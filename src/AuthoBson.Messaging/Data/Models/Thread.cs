82398e2ab7fa0ea499b6af725f7a6771486595fe
Pomidorka1234 <Jansafronov@gmail.com>
2021-11-18 17:09:23 +0200

    Commit to fixes

--- src/AuthoBson.Messaging/Data/Models/Thread.cs
+++ src/AuthoBson.Messaging/Data/Models/Thread.cs
@@ -14,26 +14,26 @@ namespace AuthoBson.Messaging.Data.Models
     [MessageIn(typeof(Thread))]
     public interface IThread : IModelBase
     {
-        [BsonElement("Content")]
-        [JsonProperty("Content")]
         Message Content { get; set; }
 
-        [BsonElement("CreatedAt")]
-        [JsonProperty("CreatedAt")]
-        [BsonRepresentation(BsonType.DateTime)]
         DateTime CreatedAt { get; }
 
-        [BsonElement("Messages")]
-        [JsonProperty("Messages")]
         IEnumerable<Message> Messages { get; set; }
     }
 
     public class Thread : ModelBase, IThread
     {
+        [BsonElement("Content")]
+        [JsonProperty("Content")]
         public Message Content { get; set; }
 
+        [BsonElement("CreatedAt")]
+        [JsonProperty("CreatedAt")]
+        [BsonRepresentation(BsonType.DateTime)]
         public DateTime CreatedAt { get; }
 
+        [BsonElement("Messages")]
+        [JsonProperty("Messages")]
         public IEnumerable<Message> Messages { get; set; }
 
         [BsonConstructor("Content", "Messages")]
